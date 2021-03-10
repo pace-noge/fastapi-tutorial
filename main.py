fb6063eb12c0b04a66e67908bcc361965236332d
Nasaruddin <nasaruddin@skorpoints.com>
2021-03-10 14:23:11 +0700

    request body + path parameters

--- main.py
+++ main.py
@@ -85,11 +85,6 @@ async def get_model(model_name: ModelName):
 async def read_file(file_path: str):
     return {"file_path": file_path}
 
-@app.post("/items/")
-async def create_item(item: Item):
-    item_dict = item.dict()
-    if item.tax:
-        price_with_tax = item.price + item.tax
-        item_dict.update({"price_with_tax": price_with_tax})
-    return item_dict
-
+@app.put("/items/{item_id}")
+async def create_item(item_id: int, item: Item):
+    return {"item_id": item_id, **item.dict()}
