06dafe5c2a0b91a0f293a51522edd3ff5e515dc9
Nasaruddin <nasaruddin@skorpoints.com>
2021-03-10 14:35:40 +0700

    add more validation

--- main.py
+++ main.py
@@ -46,7 +46,7 @@ async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
 
 
 @app.get("/items/")
-async def read_items(q: Optional[str] = Query(None, max_length=50)):
+async def read_items(q: Optional[str] = Query(None, min_length=3, max_length=50)):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
