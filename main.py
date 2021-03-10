114b11681ba3d76cf00d4a7145e6ea7dc5277f8a
Nasaruddin <nasaruddin@skorpoints.com>
2021-03-10 14:37:49 +0700

    add regular expressions

--- main.py
+++ main.py
@@ -46,7 +46,10 @@ async def read_item(item_id: str, q: Optional[str] = None, short: bool = False):
 
 
 @app.get("/items/")
-async def read_items(q: Optional[str] = Query(None, min_length=3, max_length=50)):
+async def read_items(
+    q: Optional[str] = Query(
+        None, min_length=3, max_length=50, regex="^fixedquery$")
+):
     results = {"items": [{"item_id": "Foo"}, {"item_id": "Bar"}]}
     if q:
         results.update({"q": q})
