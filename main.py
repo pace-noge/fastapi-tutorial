30535cab5b20f16bd709c193b7dc6765361489ba
Nasaruddin <nasaruddin@skorpoints.com>
2021-03-12 14:31:01 +0700

    mix path query parameters

--- main.py
+++ main.py
@@ -121,10 +121,11 @@ async def create_item(item_id: int, item: Item, q: Optional[str] = None):
 @app.put('/items/{item_id}')
 async def update_item(
     *,
-    item_id: int,
-    item: Item = Body(..., embed=True),
+    item_id: int = Path(..., title="The ID of the item to get", ge=0, le=1000),
+    item: Optional[Item] = None,
+    q: Optional[str] = None
 ):
-    results = {"item_id": item_id, "item": item, "user": user, "body": body}
+    results = {"item_id": item_id}
     if q:
         results.update({"q": q})
     return results
